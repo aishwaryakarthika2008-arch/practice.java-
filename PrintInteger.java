36ee9ccad5d12020ee08bd7249b96d4f4950444e
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-17 22:30:59 +0530

    Add PrintInteger class to read and print an integer

---
+++ PrintInteger.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PrintInteger{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       int value = sc.nextInt();
+        System.out.println(value);
+    }
+}

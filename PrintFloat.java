848c74621d4f5a15c3d234858e718b4a03ad0537
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-17 22:32:43 +0530

    Add PrintFloat class to read and print a float value

---
+++ PrintFloat.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PrintFloat{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float value = sc.nextFloat();
+        System.out.println(value);
+    }
+}

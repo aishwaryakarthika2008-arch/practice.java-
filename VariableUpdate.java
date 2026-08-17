4f6462e0678bf8aaa0a0bed1817fac9f8e685084
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-17 22:10:01 +0530

    Rename class Main to VariableUpdate

---
+++ VariableUpdate.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class VariableUpdate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        value = sc.nextInt();
+        System.out.println(value);
+    }
+}

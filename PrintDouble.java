8deeeef37e977bdf389d164d4bf1b91c45468065
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-17 22:34:23 +0530

    Add PrintDouble class to read and print a double value

---
+++ PrintDouble.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PrintDouble{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double value = sc.nextDouble();
+        System.out.println(value);
+    }
+}

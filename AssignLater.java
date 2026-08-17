40d45810a265b6588c1ae451b0ebd5b9100783dc
aishwarya karunanidhi <aishwaryakarthika2008@gmail.com>
2026-08-17 22:29:14 +0530

    Create AssignLater.java to read and print an integer

---
+++ AssignLater.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class AssignLater{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int value;
+        value = sc.nextInt();
+        System.out.println(value);
+    }
+}
